09947b1bbf448892c7653cd6cb30eb13f2505e64
Bjørn Uhre Arnholtz <rugkiks@gmail.com>
2011-11-30 12:39:33 +0100

    Derping

--- portal.py
+++ portal.py
@@ -1,14 +1,37 @@
 #!/usr/bin/python2
 # -*- coding: utf-8 -*-
+import app.config
 import env
+import time
 import wsgiserver
 
 from app.application import Application
+from app.utils.ip import getip
+from threading import Thread
 
-app = Application(debug=False)
+config = app.config.get()
+
+webapp = Application(debug=False)
+lan_ip = getip(config["interface_lan"])
+
 bind_address = "0.0.0.0"
-port = 5000
+redirecter_port = 4000
+app_port = 5000
 
-server = wsgiserver.CherryPyWSGIServer((bind_address, port), app)
+webapp = wsgiserver.CherryPyWSGIServer((bind_address, app_port), webapp)
+def redirecter(env, start_r):
+        start_r('307 Temporary Redirect', [('Location', 'http://' + lan_ip + ":5000/")])
+        return []
 
-server.start()
+redirecter = wsgiserver.CherryPyWSGIServer((bind_address, redirecter_port), redirecter)
+
+Thread(target=webapp.start).start()
+Thread(target=redirecter.start).start()
+
+try:
+    while True:
+        time.sleep(999)
+except KeyboardInterrupt:
+    webapp.stop()
+    redirecter.stop()
+
